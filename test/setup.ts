0bc36cdccb6e91b874e77c2bcf53fe7ea252548b
magistrigusti <magisrigusti@gmail.com>
2024-11-15 14:30:26 +0200

    0.1a

---
+++ test/setup.ts
@@ -0,0 +1,8 @@
+import { loadFixture } from "@nomicfoundation/hardhat-toolbox/network-helpers";
+import { ethers } from "hardhat";
+
+import { expect } from "chai";
+import '@nomicfoundation/hardhat-chai-matchers';
+
+
+export { loadFixture, ethers, expect}
