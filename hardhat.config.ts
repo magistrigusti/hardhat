eaca5cd92cb8dffd69f1b1aba1d58286777f79a6
magistrigusti <47912728+magistrigusti@users.noreply.github.com>
2024-12-11 09:47:09 +0200

    0.2

--- hardhat.config.ts
+++ hardhat.config.ts
@@ -1,8 +1,18 @@
 import { HardhatUserConfig } from "hardhat/config";
 import "@nomicfoundation/hardhat-toolbox";
+import "./tasks/sample_tasks";
 
 const config: HardhatUserConfig = {
   solidity: "0.8.27",
+  networks: {
+    hardhat: {
+      chainId: 133
+    }
+  },
+  // namedAccounts: {
+  //   deployer: 0,
+  //   user: 1,
+  // }
 };
 
 export default config;
