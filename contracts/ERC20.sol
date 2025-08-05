cc0392fbf39e9935f89d3c2cfd688fdf3aef8851
magistrigusti <47912728+magistrigusti@users.noreply.github.com>
2025-08-05 14:42:01 +0300

    0.6

---
+++ contracts/ERC20.sol
@@ -0,0 +1,9 @@
+// SPDX-License-Identifier: UNLICENSED
+pragma solidity ^0.8.28;
+
+import "./IERC20Metadata.sol";
+import "./IERC20.sol";
+
+abstract contract ERC20 is IERC20, IERC20Metadata {
+  
+}
